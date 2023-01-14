c872071d81eecafb204fbe6d1316ac72257ca5f8
Vasily Parshentsev <vparshentsev@gmail.com>
2023-01-14 09:35:49 +0300

    Initial commit

--- src/test/java/AppCardDeliveryTest.java
+++ src/test/java/AppCardDeliveryTest.java
@@ -18,15 +18,9 @@ public class AppCardDeliveryTest {
     void shouldBeSuccessfullyCompleted() {
         open("http://localhost:9999");
         String city = "Санкт-Петербург";
-        int daysToAdd = 7;
         int defaultAddedDays = 3;
-        $("[data-test-id='city'] input").setValue(city.substring(0,2));
-        $$(".menu-item__control").findBy(text(city)).click();
-        $("[data-test-id='date'] input").click();
-        if(!generateDate(defaultAddedDays, "MM").equals(generateDate(daysToAdd, "MM"))) {
-            $("[data-step='1']").click();
-        }
-        $$(".calendar__day").findBy(text(generateDate(daysToAdd, "d"))).click();
+        $("[data-test-id='city'] input").setValue(city);
+        $("[data-test-id='date'] input").setValue(generateDate(defaultAddedDays, "dd.MM.yyyy"));
         $("[data-test-id='name'] input").setValue("Иванов Иван Иванович");
         $("[data-test-id='phone'] input").setValue("+78005553535");
         $("[data-test-id='agreement']").click();
@@ -34,7 +28,7 @@ public class AppCardDeliveryTest {
         $(".notification__content")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.exactText("Встреча успешно забронирована на " +
-                        generateDate(daysToAdd, "dd.MM.yyyy")));
+                        generateDate(defaultAddedDays, "dd.MM.yyyy")));
 
     }
 }
